5f4a696964f8862751903bd18824d603779a20ac
DragonH <a0261038@g2.usc.edu.tw>
2020-05-18 02:05:03 +0800

    Add pokemon-list.json

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
+const crypto = require('crypto');
 const config = require('./config');
+const fs = require('fs');
 
 const sleep = (
   time
@@ -11,12 +13,39 @@ const sleep = (
   })
 }
 
+const saveImage = async (url) => {
+  if (!fs.existsSync('images')) {
+    fs.mkdirSync('images');
+  }
+  const binaryImage = (await axios.default.get(url, { responseType: 'arraybuffer' })).data;
+  const image = Buffer.from(binaryImage, 'binary').toString('base64')
+  const md5 = crypto.createHash('md5')
+  const imageHash = md5.update(image).digest('hex');
+  const pokemonList = JSON.parse(fs.readFileSync('./pokemon-list.json'));
+  if (Object.keys(pokemonList).indexOf(imageHash) === -1) {
+    fs.writeFileSync(`./images/${imageHash}.jpg`, binaryImage, 'binary', () => {});
+    pokemonList[imageHash] = '';
+    fs.writeFileSync(`pokemon-list.json`, JSON.stringify(pokemonList), () => {});
+    console.log(`[Message]: New pokemon image saved`);
+  }
+}
+
 const getLatestMessage = async (
   headers
 ) => {
   const response = await axios.default.get(config.url, { headers });
   if (response.data[0].embeds.length) {
     console.log(`[Message]: ${response.data[0].embeds[0].description}`);
+    
+    if (response.data[0].embeds[0].description.startsWith('Guess the pokémon')) {
+      await saveImage(response.data[0].embeds[0].image.url);
+      // let data = {
+      //   "content": "@everyone 抓寶囉 計時60秒",
+      //   "tts": false
+      // };
+      // await axios.default.post(config.url, data, { headers });
+      // await sleep(60000)
+    }
   }
   if (response.data[0].author.bot && response.data[0].content) {
     console.log(`[Message]: ${response.data[0].content}`);
@@ -48,8 +77,8 @@ const main = async () => {
 (async () => {
   try {
     main();
-  } catch (err) {
-    print(err)
+  } catch (error) {
+    console.log(`[Error]: ${error}`);
     main();
   }
 })()
