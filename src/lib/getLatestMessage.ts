3aca1fb0f985b20998c31a6c3f13de54b800f616
DragonH <a0261038@g2.usc.edu.tw>
2020-05-22 09:19:55 +0800

    Update pokemon-list.json

--- src/lib/getLatestMessage.ts
+++ src/lib/getLatestMessage.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { IHeader, IConfig } from '../types';
 import saveImage from './saveImage';
+import sleep from './sleep';
 
 export default async (
     headers: IHeader,
@@ -11,12 +12,12 @@ export default async (
         console.log(`[Message]: ${response.data[0].embeds[0].description}`);
         if (response.data[0].embeds[0].description.startsWith('Guess the pokémon')) {
             await saveImage(response.data[0].embeds[0].image.url);
-            // let data = {
-            //   "content": "@everyone 抓寶囉 計時60秒",
-            //   "tts": false
+            // const data = {
+            //     content: '@everyone 抓寶囉 計時60秒',
+            //     tts: false,
             // };
-            // await axios.default.post(config.url, data, { headers });
-            // await sleep(60000)
+            // await axios.post(config.url, data, { headers });
+            // await sleep(60000);
         }
     }
     if (response.data[0].author.bot && response.data[0].content) {
